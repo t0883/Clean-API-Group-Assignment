0fa06238704a0cf1ced51b0b1bc36632a079d2ba
t0883 <tobiiasa@hotmail.com>
2024-01-16 14:06:35 +0100

    Added test for get all brand (#54)
    Co-authored-by: Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>

---
+++ Test/BrandTests/QueryTest/GetAllBrandsTests.cs
@@ -0,0 +1,35 @@
+﻿using Application.Queries.Brands.GetAll;
+using Domain.Models.Brands;
+using FakeItEasy;
+using Infrastructure.Repository.Brands;
+
+namespace Test.BrandTests.QueryTest
+{
+    [TestFixture]
+    internal class GetAllBrandsTests
+    {
+        [Test]
+        public async Task Get_List_Of_Brands_Should_Return_List_Of_Brands()
+        {
+
+            //Arrange
+            var testRepository = A.Fake<IBrandRepository>();
+
+            var query = new GetAllBrandsQuery();
+
+            var queryHandler = new GetAllBrandsQueryHandler(testRepository);
+
+            A.CallTo(() => testRepository.GetAllBrands()).Returns(new List<Brand> { new Brand { BrandId = new Guid(), BrandName = "test1" }, new Brand { BrandId = new Guid(), BrandName = "test2" }, new Brand { BrandId = new Guid(), BrandName = "test3" } });
+
+            //Act
+
+            var result = await queryHandler.Handle(query, CancellationToken.None);
+
+            //Assert
+
+            Assert.IsNotNull(result);
+            Assert.That(result.Count(), Is.EqualTo(3));
+            Assert.That(result, Is.TypeOf<List<Brand>>());
+        }
+    }
+}
