4a3ceb96ba9ade6fd35138879f1c19db9612874c
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2024-01-17 15:33:57 +0100

    Added httppost for user. Added command and handler. Added UserDto. Added UserRepository with interface. Added it to DependencyInjection.

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastructure.Repository.Brands;
+using Infrastructure.Repository.Users;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
@@ -8,6 +9,7 @@ namespace Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddScoped<IBrandRepository, BrandRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
 
             return services;
         }
---
+++ Infrastructure/Repository/Users/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models.Users;
+
+namespace Infrastructure.Repository.Users
+{
+    public interface IUserRepository
+    {
+        Task<User> AddUser(User user);
+    }
+}
