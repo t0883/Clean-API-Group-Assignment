14d4ffa0cdf07520a90f87d2072ef0a49421c2e4
t0883 <tobiiasa@hotmail.com>
2024-01-17 10:54:12 +0100

    Installed FluentValidation and added it to DependencyInjection (#58)
    Co-authored-by: Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Application
 {
@@ -9,6 +10,8 @@ namespace Application
             var assembly = typeof(DependencyInjection).Assembly;
 
             services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
 
 
             return services;
