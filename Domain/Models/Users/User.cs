954da0acdb69b7cb5134b2db8f125736ced1bf49
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2024-01-17 14:32:01 +0100

    Created User model

---
+++ Domain/Models/Users/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models.Users
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
