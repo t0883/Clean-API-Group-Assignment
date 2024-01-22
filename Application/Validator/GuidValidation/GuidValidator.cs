0d70b2383359d290aac57ef1959d2f85ceb896fa
t0883 <tobiiasa@hotmail.com>
2024-01-22 19:17:59 +0100

    Added guid validator. (#128)
    Added validator to brandcontroller.
    Cleaned up .csproj
    
    Co-authored-by: Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>

---
+++ Application/Validator/GuidValidation/GuidValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Validator.GuidValidation
+{
+    public class GuidValidator : AbstractValidator<Guid>
+    {
+        public GuidValidator()
+        {
+            RuleFor(guid => guid).NotNull().WithMessage("Guid cannot be null").NotEmpty().WithMessage("Guid cannot be empty").NotEqual(Guid.Empty).WithMessage("Thats not a guid");
+        }
+
+    }
+}
