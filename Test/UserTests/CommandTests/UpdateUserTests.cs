d37f5df5388a4723080da8f4bd8d3fe6991e9c92
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2024-01-25 15:42:02 +0100

    Added test for update user. Changed UpdateUserCommandHandler to public.

---
+++ Test/UserTests/CommandTests/UpdateUserTests.cs
@@ -0,0 +1,36 @@
+﻿using Application.Commands.Users.UpdateUser;
+using Application.Dtos;
+using Domain.Models.Users;
+using FakeItEasy;
+using Infrastructure.Repository.Users;
+namespace Test.UserTests.CommandTests
+{
+    [TestFixture]
+    internal class UpdateUserTests
+    {
+        [Test]
+        public async Task Update_User_Should_Return_User_With_Username_test()
+        {
+
+            //Arrange
+            UserDto userDto = new UserDto { Email = "test@test.com", Password = "password", Username = "Test" };
+
+            var fakeRepository = A.Fake<IUserRepository>();
+
+            var command = new UpdateUserCommand(userDto);
+
+            var commandHandler = new UpdateUserCommandHandler(fakeRepository);
+
+            A.CallTo(() => fakeRepository.UpdateUser(A<User>._)).Returns(new User { Email = "test@test.com", Password = "password", Username = "test", UserId = Guid.NewGuid() });
+
+            //Act
+
+            var result = await commandHandler.Handle(command, CancellationToken.None);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.Username.Equals("test"));
+            Assert.That(result.GetType(), Is.EqualTo(typeof(User)));
+        }
+    }
+}
