797f90ad38879136727a0774134a15d7b3fe8145
FTrajanovski <115001522+FTrajanovski@users.noreply.github.com>
2024-01-26 08:30:44 +0100

    Feature/169 tire test get by (#175)
    * Added Test for DeleteTire
    
    * Added test for UpdateTire
    
    * Added test for GetAllTires
    
    * Added test for GetTireByBrand
    
    * Added test to GetTireById

---
+++ Test/TireTests/CommandTests/DeleteTireTests.cs
@@ -0,0 +1,32 @@
+﻿using Application.Commands.Tires.DeleteTire;
+using FakeItEasy;
+using Infrastructure.Repository.Tires;
+using MediatR;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test.TireTests.CommandTests
+{
+    [TestFixture]
+    internal class DeleteTireTests
+    {
+        [Test]
+        public async Task Delete_Tire_By_Id_Should_Return_Unit()
+        {
+            // Arrange
+            var tireIdToDelete = Guid.NewGuid();
+            var testRepository = A.Fake<ITireRepository>();
+            var command = new DeleteTireByIdCommand(tireIdToDelete);
+            var commandHandler = new DeleteTireByIdCommandHandler(testRepository);
+
+            // Act
+            var result = await commandHandler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(Unit.Value));
+            A.CallTo(() => testRepository.DeleteTireById(tireIdToDelete)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
---
+++ Test/TireTests/QueryTests/GetTireByBrandTests.cs
@@ -0,0 +1,47 @@
+﻿using Application.Queries.Tires.GetByBrand;
+using Domain.Models.Brands;
+using Domain.Models.Tires;
+using FakeItEasy;
+using Infrastructure.Repository.Tires;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test.TireTests.QueryTests
+{
+    [TestFixture]
+    internal class GetTireByBrandTest
+    {
+        [Test]
+        public async Task Get_Tire_By_Brand_Should_Return_List_Of_Tires()
+        {
+            // Arrange
+            string testBrandName = "TestBrand";
+            var testTires = new List<Tire>
+            {
+                new Tire { TireId = Guid.NewGuid(), TireModel = "TestModel1", Brand = new Brand { BrandName = testBrandName }, TireSize = "TestSize1", TireTreadDepth = 10.5m },
+                new Tire { TireId = Guid.NewGuid(), TireModel = "TestModel2", Brand = new Brand { BrandName = testBrandName }, TireSize = "TestSize2", TireTreadDepth = 11.5m },
+                // Add more test tires for the same brand or different brands as needed
+            };
+
+            var testRepository = A.Fake<ITireRepository>();
+            A.CallTo(() => testRepository.GetTireByBrand(testBrandName)).Returns(testTires);
+
+            var query = new GetTireByBrandQuery(testBrandName);
+            var queryHandler = new GetTireByBrandQueryHandler(testRepository);
+
+            // Act
+            var result = await queryHandler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<List<Tire>>(result);
+            Assert.That(result.Count, Is.EqualTo(testTires.Count)); // Ensure the returned list has the same count as the test data
+            foreach (var tire in result)
+            {
+                Assert.That(tire.Brand.BrandName, Is.EqualTo(testBrandName));
+            }
+        }
+    }
+}
