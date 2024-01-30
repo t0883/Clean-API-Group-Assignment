3243907ee0d8b3343ea58b8a66aee49f0555b007
AdamHjalmarsson99 <115019444+AdamHjalmarsson99@users.noreply.github.com>
2024-01-30 21:51:54 +0100

    Added Validation and some errorhandling for Gearbox. (#197)

---
+++ Application/Validator/GearboxModelValidation/GearboxModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Application.Validator.GearboxModelValidation
+{
+    public class GearboxModelValidator : AbstractValidator<string>
+    {
+        public GearboxModelValidator()
+        {
+            RuleFor(gearboxModel => gearboxModel)
+                .Must(BeAValidGearboxModel).WithMessage("Invalid gearbox model! Must be 'Manual' or 'Automatic'!");
+        }
+
+        private bool BeAValidGearboxModel(string gearboxModel)
+        {
+            // Här kan du definiera de giltiga värdena för gearboxModel
+            return gearboxModel == "Manual" || gearboxModel == "Automatic";
+        }
+
+    }
+}
