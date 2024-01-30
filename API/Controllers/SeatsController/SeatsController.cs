da92df433e8704e5bf902a437d580d64db2c65ab
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2024-01-30 21:50:53 +0100

    Added seat controller

---
+++ API/Controllers/SeatsController/SeatsController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.SeatsController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeatsController : ControllerBase
+    {
+        internal readonly IMediator _mediator;
+        public SeatsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
