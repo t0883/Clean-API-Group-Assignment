7d2ebc2560de7db44a08a8f9c60d2b03be220edd
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2024-01-30 23:28:44 +0100

    Added commands for delete seat

---
+++ Application/Commands/Seats/DeleteSeat/DeleteSeatByIdCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Seats.DeleteSeat
+{
+    public class DeleteSeatByIdCommand : IRequest<Unit>
+    {
+        public DeleteSeatByIdCommand(Guid seatId)
+        {
+            SeatId = seatId;
+        }
+
+        public Guid SeatId { get; }
+    }
+}
---
+++ Application/Commands/Seats/DeleteSeat/DeleteSeatByIdCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Repository.Seats;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Seats.DeleteSeat
+{
+    public class DeleteSeatByIdCommandHandler : IRequestHandler<DeleteSeatByIdCommand, Unit>
+    {
+        private readonly ISeatRepository _seatRepository;
+
+        public DeleteSeatByIdCommandHandler(ISeatRepository seatRepository)
+        {
+            _seatRepository = seatRepository ?? throw new ArgumentNullException(nameof(seatRepository));
+        }
+
+        public async Task<Unit> Handle(DeleteSeatByIdCommand request, CancellationToken cancellationToken)
+        {
+            await _seatRepository.DeleteSeatById(request.SeatId);
+
+            return Unit.Value;
+        }
+    }
