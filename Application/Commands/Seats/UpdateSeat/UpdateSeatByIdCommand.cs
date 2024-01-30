b75d87bbd7f4971442704296ff816c1c34d8d56a
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2024-01-30 23:40:23 +0100

    Added commands for update seat

---
+++ Application/Commands/Seats/UpdateSeat/UpdateSeatByIdCommand.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Core;
+using Domain.Models.Brands;
+using Domain.Models.Seats;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Seats.UpdateSeat
+{
+    public class UpdateSeatByIdCommand : IRequest<Unit>
+    {
+        public UpdateSeatByIdCommand(Seat seatToUpdate)
+        {
+            SeatName = seatToUpdate.SeatName;
+            Brand = seatToUpdate.Brand;
+            SeatColor = seatToUpdate.SeatColor;
+            SeatMaterial = seatToUpdate.SeatMaterial;
+        }
+
+        public string SeatName { get; }
+        public Brand Brand { get; }
+        public string SeatColor { get; }
+        public string SeatMaterial { get; }
+    }
+}
---
+++ Application/Commands/Seats/UpdateSeat/UpdateSeatByIdCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Commands.Seats.UpdateSeat;
+using Domain.Models.Seats;
+using Infrastructure.Repository.Seats;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Seats.UpdateSeat
+{
+    public class UpdateSeatByIdCommandHandler : IRequestHandler<UpdateSeatByIdCommand, Unit>
+    {
+        private readonly ISeatRepository _seatRepository;
+
+        public UpdateSeatByIdCommandHandler(ISeatRepository seatRepository)
+        {
+            _seatRepository = seatRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateSeatByIdCommand request, CancellationToken cancellationToken)
+        {
+            var updatedSeat = new Seat
+            {
+                SeatName = request.SeatName,
+                Brand = request.Brand,
+                SeatColor = request.SeatColor,
+                SeatMaterial = request.SeatMaterial
+            };
+
+            await _seatRepository.UpdateSeat(updatedSeat);
+
+            return Unit.Value;
+        }
+    }
