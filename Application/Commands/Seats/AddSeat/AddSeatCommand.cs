fd27004a2e58cc90d5fcb73476b2e00f3d7a88a4
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2024-01-30 23:22:16 +0100

    Added commands for add seat

---
+++ Application/Commands/Seats/AddSeat/AddSeatCommand.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Seats.AddSeat
+{
+    public class AddSeatCommand : IRequest<Unit>
+    {
+        public AddSeatCommand(SeatDto newSeat)
+        {
+            NewSeat = newSeat;
+        }
+
+        public SeatDto NewSeat { get; }
+    }
+}
---
+++ Application/Commands/Seats/AddSeat/AddSeatCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Models.Seats;
+using Infrastructure.Repository.Seats;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Seats.AddSeat
+{
+    public class AddSeatCommandHandler : IRequestHandler<AddSeatCommand, Unit>
+    {
+        private readonly ISeatRepository _seatRepository;
+
+        public AddSeatCommandHandler(ISeatRepository seatRepository)
+        {
+            _seatRepository = seatRepository ?? throw new ArgumentNullException(nameof(seatRepository));
+        }
+
+        public async Task<Unit> Handle(AddSeatCommand request, CancellationToken cancellationToken)
+        {
+            var newSeat= new Seat
+            {
+                SeatName = request.NewSeat.SeatName,
+                Brand = request.NewSeat.Brand,
+                SeatColor = request.NewSeat.SeatColor,
+                SeatMaterial = request.NewSeat.SeatMaterial
+            };
+
+            await _seatRepository.AddSeat(newSeat);
+
+            return Unit.Value;
+        }
+    }
+}
