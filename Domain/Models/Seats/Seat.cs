f27d1a29d3afca77449914e43dee47b2ed64ddb3
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2024-01-30 21:41:24 +0100

    Added modell for seat

---
+++ Domain/Models/Seats/Seat.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models.Brands;
+
+namespace Domain.Models.Seats
+{
+    public class Seat
+    {
+        public Guid SeatId { get; set; }
+        public string SeatName { get; set; }
+        public Brand Brand { get; set; }
+        public string SeatColor { get; set; }
+        public string SeatMaterial { get; set; }
+    }
+}
