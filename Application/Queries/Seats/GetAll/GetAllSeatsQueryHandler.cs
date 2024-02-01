387e53a7638abb621ef666e4ab045d315b0c2e7a
Gabriella Aslan <119976958+GabrielllaAslan@users.noreply.github.com>
2024-02-01 07:17:21 +0100

    Added query and query handler for seats. (#200)

---
+++ Application/Queries/Seats/GetAll/GetAllSeatsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Queries.Seats.GetAll;
+using Domain.Models.Seats;
+using Infrastructure.Repository.Seats;
+using MediatR;
+
+namespace Application.Queries.Seats.GetAll
+{
+    public class GetAllSeatsQueryHandler : IRequestHandler<GetAllSeatsQuery, List<Seat>>
+    {
+        private readonly ISeatRepository _seatRepository;
+
+        public GetAllSeatsQueryHandler(ISeatRepository seatRepository)
+        {
+            _seatRepository = seatRepository;
+        }
+
+        public async Task<List<Seat>> Handle(GetAllSeatsQuery request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(await _seatRepository.GetAllSeats());
+        }
+    }
+}
