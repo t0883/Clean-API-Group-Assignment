53f942405aab5f724c2c760f2da30694539a731d
Emelia Sundelund <105558496+EmeliaSundelund@users.noreply.github.com>
2024-02-01 19:38:58 +0100

    Feature/184 added error engine controller (#209)
    * added errorhandeling in enginecontroller
    
    * Update EngineController.cs

--- API/Controllers/EngineController/EngineController.cs
+++ API/Controllers/EngineController/EngineController.cs
@@ -30,35 +30,28 @@ namespace API.Controllers.EnginesController
             _stringValidator = stringValidator;
             _intValidator = intValidator;
         }
-
         [HttpPost]
         [Route("addNewEngine")]
         public async Task<IActionResult> AddEngine([FromBody] EngineDto engine)
         {
             var validatedEngineName = _stringValidator.Validate(engine.EngineName);
-
             if (!validatedEngineName.IsValid)
             {
                 return BadRequest(validatedEngineName.Errors.ConvertAll(errors => errors.ErrorMessage));
             }
-
             var validatedHorsePower = _intValidator.Validate(engine.HorsePower);
-
             if (!validatedHorsePower.IsValid)
             {
                 return BadRequest(validatedHorsePower.Errors.ConvertAll(errors => errors.ErrorMessage));
             }
-
             return Ok(await _mediator.Send(new AddEngineCommand(engine)));
         }
-
         [HttpGet]
         [Route("getAllEngine")]
         public async Task<IActionResult> GetAllEngines()
         {
             return Ok(await _mediator.Send(new GetAllEngineQuery()));
         }
-
         [HttpGet]
         [Route("getEngineById")]
         public async Task<IActionResult> GetEngineById(Guid engineId)
@@ -69,10 +62,8 @@ namespace API.Controllers.EnginesController
             {
                 return BadRequest(validatedEngineId.Errors.Select(error => error.ErrorMessage));
             }
-
             return Ok(await _mediator.Send(new GetEngineByIdQuery(engineId)));
         }
-
         [HttpPut]
         [Route("updateEngine")]
         public async Task<IActionResult> UpdateEngine([FromBody] Engine engineToUpdate)
@@ -80,19 +71,15 @@ namespace API.Controllers.EnginesController
             try
             {
                 var validatedEngineId = _guidValidator.Validate(engineToUpdate.EngineId);
-
                 if (!validatedEngineId.IsValid)
                 {
                     return BadRequest(validatedEngineId.Errors.ConvertAll(errors => errors.ErrorMessage));
                 }
-
                 var validatedHorsePower = _intValidator.Validate(engineToUpdate.HorsePower);
-
                 if (!validatedHorsePower.IsValid)
                 {
                     return BadRequest(validatedHorsePower.Errors.ConvertAll(errors => errors.ErrorMessage));
                 }
-
                 return Ok(await _mediator.Send(new UpdateEngineCommand(engineToUpdate)));
             }
             catch (Exception ex)
@@ -100,21 +87,17 @@ namespace API.Controllers.EnginesController
                 throw new ArgumentException(ex.Message);
             }
         }
-
         [HttpDelete]
         [Route("deleteEngine")]
         public async Task<IActionResult> DeleteEngine(Guid engineId)
         {
             var validatedEngineId = _guidValidator.Validate(engineId);
-
             if (!validatedEngineId.IsValid)
             {
                 return BadRequest(validatedEngineId.Errors.Select(error => error.ErrorMessage));
             }
-
             await _mediator.Send(new DeleteEngineCommand(engineId));
             return NoContent();
         }
-
     }
 }
