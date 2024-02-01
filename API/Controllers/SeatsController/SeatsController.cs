8dac5ca253bf210023645fd3aeeb3c100e258029
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2024-02-01 20:09:56 +0100

    Added HttpPost for seat + validator

--- API/Controllers/SeatsController/SeatsController.cs
+++ API/Controllers/SeatsController/SeatsController.cs
@@ -1,4 +1,8 @@
-﻿using MediatR;
+﻿using Application.Commands.Seats.AddSeat;
+using Application.Dtos;
+using Application.Validator.GuidValidation;
+using Application.Validator.StringValidation;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers.SeatsController
@@ -8,9 +12,37 @@ namespace API.Controllers.SeatsController
     public class SeatsController : ControllerBase
     {
         internal readonly IMediator _mediator;
-        public SeatsController(IMediator mediator)
+        internal readonly GuidValidator _guidValidator;
+        internal readonly StringValidator _stringValidator;
+        public SeatsController(IMediator mediator, GuidValidator guidValidator, StringValidator stringValidator)
         {
             _mediator = mediator;
+            _guidValidator = guidValidator;
+            _stringValidator = stringValidator;
+        }
+
+        [HttpPost]
+        [Route("addNewSeat")]
+        public async Task<IActionResult> AddSeat([FromBody] SeatDto seat)
+        {
+            var validatedSeatName = _stringValidator.Validate(seat.SeatName);
+            if (!validatedSeatName.IsValid)
+            {
+                return BadRequest(validatedSeatName.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+            
+            var validatedSeatColor = _stringValidator.Validate(seat.SeatColor);
+            if (!validatedSeatColor.IsValid)
+            {
+                return BadRequest(validatedSeatColor.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+           
+            var validatedSeatMaterial = _stringValidator.Validate(seat.SeatMaterial);
+            if (!validatedSeatMaterial.IsValid)
+            {
+                return BadRequest(validatedSeatMaterial.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+            return Ok(await _mediator.Send(new AddSeatCommand(seat)));
         }
     }
 }
