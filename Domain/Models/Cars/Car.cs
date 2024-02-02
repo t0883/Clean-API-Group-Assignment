222a6fadafc5e56a3f4a187a0c757fb813d316ca
t0883 <tobiiasa@hotmail.com>
2024-02-02 14:26:55 +0100

    Added car model to database. Added migration and added annotations for all the fkeys in the car model. (#213)
    Co-authored-by: Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>

--- Domain/Models/Cars/Car.cs
+++ Domain/Models/Cars/Car.cs
@@ -4,6 +4,7 @@ using Domain.Models.Gearboxes;
 using Domain.Models.Seats;
 using Domain.Models.Tires;
 using Domain.Models.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Models.Cars
 {
@@ -11,13 +12,19 @@ namespace Domain.Models.Cars
     {
         public Guid CarId { get; set; }
         public string Name { get; set; }
+        [DeleteBehavior(DeleteBehavior.Restrict)]
         public Brand Brand { get; set; }
+        [DeleteBehavior(DeleteBehavior.Restrict)]
         public Gearbox Gearbox { get; set; }
+        [DeleteBehavior(DeleteBehavior.Restrict)]
         public Tire Tire { get; set; }
+        [DeleteBehavior(DeleteBehavior.Restrict)]
         public Engine Engine { get; set; }
+        [DeleteBehavior(DeleteBehavior.Restrict)]
         public Seat Seat { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+        [DeleteBehavior(DeleteBehavior.Restrict)]
         public User CreatedBy { get; set; }
     }
 }
