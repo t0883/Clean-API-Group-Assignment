71cc73c2b24ab80be4e9e6aa2be7ee748c0194b4
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2024-02-04 09:30:25 +0100

    Added a dto for car.

---
+++ Application/Dtos/CarDto.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models.Brands;
+using Domain.Models.Engines;
+using Domain.Models.Gearboxes;
+using Domain.Models.Seats;
+using Domain.Models.Tires;
+using Domain.Models.Users;
+
+namespace Application.Dtos
+{
+    public class CarDto
+    {
+        public required string CarName { get; set; }
+        public required Brand BrandName { get; set; }
+        public required Gearbox GearboxId { get; set; }
+        public required Tire TireId { get; set; }
+        public required Engine EngineId { get; set; }
+        public required Seat SeatId { get; set; }
+        public required User UserId { get; set; }
+
+    }
+}
