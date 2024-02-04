98bf0d8d8621a890b7ef7fad031de1fd58374e94
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2024-02-04 09:36:03 +0100

    Added a controller for car.

---
+++ API/Controllers/CarsController/CarController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.CarsController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        internal readonly IMediator _mediator;
+
+        public CarController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
